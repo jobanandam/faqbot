7156c1540b3fd126fe12eca2a744eb0edaf5ab84
Balaji vijayakumar <bala111990@gmail.com>
2019-11-13 02:47:04 +0530

    Rest api integration with UI

--- application/botapi.py
+++ application/botapi.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 from faqbot import SentenceSimilarities
+
 app = Flask(__name__)
+
+cors = CORS(app, resources={r"/bot/dev-ops/<question>": {"origins": "http://localhost:8000"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/bot/dev-ops/<question>', methods=['GET'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def sentiment_index(question):
     SentenceSimilarities.perform_classification_on_test_data()
     print("Hi Welcome to FAQ BOT ")
@@ -11,7 +17,6 @@ def sentiment_index(question):
     result = SentenceSimilarities.get_questions_from_user_interface(question, False)
     print(result)
     return result
-
 
 
 if __name__ == '__main__':
--- presentation/faqbot/mybot/static/myapp/js/version2.js
+++ presentation/faqbot/mybot/static/myapp/js/version2.js
@@ -1,56 +1,70 @@
 (function() {
-        var Message;
-        Message = function(arg) {
-            this.text = arg.text, this.message_side = arg.message_side;
-            this.draw = function(_this) {
-                return function() {
-                    var $message;
-                    $message = $($('.message_template').clone().html());
-                    $message.addClass(_this.message_side).find('.text').html(_this.text);
-                    $('.messages').append($message);
-                    return setTimeout(function() {
-                        return $message.addClass('appeared');
-                    }, 0);
-                };
-            }(this);
-            return this;
+    var Message;
+    Message = function(arg) {
+        this.text = arg.text, this.message_side = arg.message_side;
+        this.draw = function(_this) {
+            return function() {
+                var $message;
+                $message = $($('.message_template').clone().html());
+                $message.addClass(_this.message_side).find('.text').html(_this.text);
+                $('.messages').append($message);
+                return setTimeout(function() {
+                    return $message.addClass('appeared');
+                }, 0);
+            };
+        }(this);
+        return this;
+    };
+    $(function() {
+        var getMessageText, message_side, sendMessage;
+        message_side = 'right';
+        getMessageText = function() {
+            var $message_input;
+            $message_input = $('.message_input');
+            return $message_input.val();
         };
-        $(function() {
-            var getMessageText, message_side, sendMessage;
-            message_side = 'right';
-            getMessageText = function() {
-                var $message_input;
-                $message_input = $('.message_input');
-                return $message_input.val();
-            };
-            sendMessage = function(text) {
-                var $messages, message;
-                if (text.trim() === '') {
-                    return;
-                }
-                $('.message_input').val('');
-                $messages = $('.messages');
-                message_side = message_side === 'left' ? 'right' : 'left';
-                message = new Message({
-                    text: text,
-                    message_side: message_side
-                });
-                message.draw();
-                return $messages.animate({
-                    scrollTop: $messages.prop('scrollHeight')
-                }, 300);
-            };
-            $('.send_message').click(function(e) {
-                return sendMessage(getMessageText());
-            });
-            $('.message_input').keyup(function(e) {
-                if (e.which === 13) {
-                    return sendMessage(getMessageText());
+        callApi = function(question){
+            $.ajax({
+                url: 'http://localhost:9080/bot/dev-ops/'+question,
+                type: 'GET',
+                cache: false,
+                processData: false,
+                contentType: false,
+                success: function(data) {
+                    console.log(data);
+                    sendMessage(data);
                 }
             });
-            sendMessage('Hello Welome !!! Myself FAQ BOT .. How can I Help you ?');
-            setTimeout(function() {
-				return sendMessage('Hello Welome !!! Myself FAQ BOT .. How can I Help you ?');
-			}, 1000);
+        };
+        sendMessage = function(text) {
+            var $messages, message;
+            if (text.trim() === '') {
+                return;
+            }
+            $('.message_input').val('');
+            $messages = $('.messages');
+            message_side = message_side === 'left' ? 'right' : 'left';
+            message = new Message({
+                text: text,
+                message_side: message_side
+            });
+            message.draw();
+            return $messages.animate({
+                scrollTop: $messages.prop('scrollHeight')
+            }, 300);
+        };
+        $('.send_message').click(function(e) {
+            question_asked= getMessageText();
+            sendMessage(question_asked);
+            return callApi(question_asked);
         });
-    }.call(this));
+        $('.message_input').keyup(function(e) {
+            if (e.which === 13) {
+                question_asked= getMessageText();
+                sendMessage(question_asked);
+                return callApi(question_asked);
+            }
+        });
+        sendMessage('Hello Welome !!! Myself FAQ BOT .. How can I Help you ?');
+    });
+}.call(this));
