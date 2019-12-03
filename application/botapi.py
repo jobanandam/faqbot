cd01b2001f844578ac9ca26e89743f801f47953d
ramnath10 <ramnathdevarajan@gmail.com>
2019-12-03 01:27:55 +0100

    Feedback mechanism - Initial commit - UI

--- application/botapi.py
+++ application/botapi.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import jsonify
+from flask import request
 from flask_cors import CORS, cross_origin
 from faqbot import SentenceSimilarities
 
@@ -11,12 +13,16 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/bot/dev-ops/<question>', methods=['GET'])
 @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def sentiment_index(question):
+    feedback = request.args.get('feedback')
+    conversation_key = request.args.get('conversation_key')
+    print(feedback + ' &&&&& ' + conversation_key + ' &&&&& ' + question)
     SentenceSimilarities.perform_classification_on_test_data()
     print("Hi Welcome to FAQ BOT ")
     print("Question asked --> ", question)
     result = SentenceSimilarities.get_questions_from_user_interface(question, False, True)
-    print(result)
-    return result
+    response = jsonify(result)
+    print(response)
+    return response
 
 
 if __name__ == '__main__':
--- presentation/faqbot/mybot/static/myapp/js/version2.js
+++ presentation/faqbot/mybot/static/myapp/js/version2.js
@@ -1,7 +1,10 @@
 (function() {
-    var Message;
+    var Message; // contains the message object which has all details about the message
     Message = function(arg) {
-        this.text = arg.text, this.message_side = arg.message_side;
+        this.text = arg.text, this.message_side = arg.message_side,
+        this.prompt_feedback = arg.prompt_feedback, this.conversation_key = arg.conversation_key,
+        this.feedback_for_question = arg.feedback_for_question,
+        this.user_feedback = arg.user_feedback;
         this.draw = function(_this) {
             return function() {
                 var $message;
@@ -13,58 +16,98 @@
                 }, 0);
             };
         }(this);
+        this.getQueryString = function(_this) {
+            return function() {
+                if (_this.feedback_for_question !== ''){
+                    return '?feedback=' + _this.user_feedback + '&conversation_key' + _this.conversation_key;
+                } else {
+                    return '';
+                }
+            };
+        }(this);
+        this.reset = function(_this) {
+            return function() {
+                _this.prompt_feedback = ''
+                _this.conversation_key = ''
+                _this.feedback_for_question = ''
+            };
+        }(this);
+        this.isFeedBack = function(_this) {
+            return function() {
+                _this.feedback_for_question !== ''
+            };
+        }(this);
         return this;
     };
     $(function() {
-        var getMessageText, message_side, sendMessage;
-        message_side = 'right';
+        var getMessageText, message_side, sendMessage, message;
+        message = new Message({ // initialize once globally
+                text: 'Hello Welcome !!! I am FAQ BOT .. How can I help you ?',
+                message_side: 'right',
+                prompt_feedback: '',
+                conversation_key: '',
+                feedback_for_question: ''
+            });
+
         getMessageText = function() {
             var $message_input;
             $message_input = $('.message_input');
             return $message_input.val();
         };
-        callApi = function(question){
+        callApi = function(message){
+            question = message.isFeedBack() ? message.feedback_for_question : message.text;
+            query_string = message.getQueryString();
             $.ajax({
-                url: 'http://localhost:9080/bot/dev-ops/'+question,
+                url: 'http://localhost:9080/bot/dev-ops/' + question + query_string,
                 type: 'GET',
                 cache: false,
                 processData: false,
                 contentType: false,
-                success: function(data) {
+                success: function(data, question) {
                     console.log(data);
-                    sendMessage(data);
+                    processMessage(data, question);
                 }
             });
         };
-        sendMessage = function(text) {
-            var $messages, message;
-            if (text.trim() === '') {
+        processMessage = function(data, question) {
+            if (data === '' || data === undefined) {
+                return;
+            }
+            message.text = data.answer;
+            if (message.prompt_feedback == 'Y'){
+                message.prompt_feedback = data.prompt_feedback;
+                message.conversation_key = data.conversation_key;
+                message.feedback_for_question = question;
+                message.text = message.text + '\n Is this the answer you are looking for? Please reply "yes" or "no". To abort, reply "stop".'
+            }
+            sendMessage(message);
+        };
+        sendMessage = function(message) {
+            var $messages;
+            if (message === '') {
                 return;
             }
             $('.message_input').val('');
             $messages = $('.messages');
-            message_side = message_side === 'left' ? 'right' : 'left';
-            message = new Message({
-                text: text,
-                message_side: message_side
-            });
+            message.message_side = message.message_side === 'left' ? 'right' : 'left';
             message.draw();
             return $messages.animate({
                 scrollTop: $messages.prop('scrollHeight')
             }, 300);
         };
         $('.send_message').click(function(e) {
-            question_asked= getMessageText();
-            sendMessage(question_asked);
-            return callApi(question_asked);
+            return processUserInput();
         });
         $('.message_input').keyup(function(e) {
             if (e.which === 13) {
-                question_asked= getMessageText();
-                sendMessage(question_asked);
-                return callApi(question_asked);
+                return processUserInput();
             }
         });
-        sendMessage('Hello Welome !!! Myself FAQ BOT .. How can I Help you ?');
+        processUserInput = function (){
+            message.text = getMessageText();
+            sendMessage(message);
+            return callApi(message);
+        };
+        sendMessage(message);
     });
 }.call(this));
