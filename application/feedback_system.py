b1be03fd868cbfa9f1dbf1d7d43f1001c45f9e1a
yuvaraj <yuvamcse@gmail.com>
2019-12-07 18:09:16 +0100

    avoided multiple returns in get_next_suggestible_question_from

--- application/feedback_system.py
+++ application/feedback_system.py
@@ -2,8 +2,7 @@ from application.file_reader import read_json_file
 from application.questions_io import update_user_suggestible_questions
 
 
-def get_next_suggestible_question_from(suggestible_questions):
-    sorted_suggestible_questions = sorted(suggestible_questions, key=lambda record: record["score"], reverse=True)
+def get_next_suggestible_question_from(user_id, sorted_suggestible_questions):
     sorted_suggestible_questions = list(sorted_suggestible_questions)
 
     for index in range(len(sorted_suggestible_questions)):
@@ -11,9 +10,12 @@ def get_next_suggestible_question_from(suggestible_questions):
         if next_suggestible_question["processed"] == "N":
             next_suggestible_question = mark_question_as_processed(next_suggestible_question)
             sorted_suggestible_questions[index] = next_suggestible_question
-            return next_suggestible_question, sorted_suggestible_questions
 
-    return (), []
+            update_user_suggestible_questions(user_id, sorted_suggestible_questions)
+
+            return next_suggestible_question
+
+    return None
 
 
 def mark_question_as_processed(sorted_suggestible_question):
@@ -23,26 +25,34 @@ def mark_question_as_processed(sorted_suggestible_question):
 
 def main():
     user_id = "4186950e-3c72-40fa-88c5-28aa278fccd6"
-    next_suggestible_question, user_suggestible_questions = get_next_suggestible_question_for(user_id)
+    next_suggestible_question = get_next_suggestible_question_for(user_id)
 
-    while next_suggestible_question != ():
-        update_user_suggestible_questions(user_id, user_suggestible_questions)
+    while next_suggestible_question.get("question") is not None:
         print("Is this what you are looking for?")
-        print(next_suggestible_question)
+        print(next_suggestible_question.get("question"))
         feedback = input()
         if feedback == "Y":
             print("Suggestion accepted: ", next_suggestible_question["question"])
+            next_suggestible_question["accepted"] = "Y"
             break
         else:
-            next_suggestible_question, user_suggestible_questions = get_next_suggestible_question_from(
-                user_suggestible_questions)
+            next_suggestible_question = get_next_suggestible_question_for(user_id)
 
 
 def get_next_suggestible_question_for(user_id):
     user_suggestible_questions = get_user_specific_suggestible_questions(user_id)
-    next_suggestible_question, user_suggestible_questions = \
-        get_next_suggestible_question_from(user_suggestible_questions)
-    return next_suggestible_question, user_suggestible_questions
+
+    no_questions_left = {"answer": ""}
+    if len(user_suggestible_questions) == 0:
+        return no_questions_left
+    else:
+        sorted_suggestible_questions = sorted(user_suggestible_questions, key=lambda record: record["score"], reverse=True)
+        next_suggestible_question = get_next_suggestible_question_from(user_id, sorted_suggestible_questions)
+
+        if next_suggestible_question is not None:
+            return next_suggestible_question
+        else:
+            return no_questions_left
 
 
 def get_user_specific_suggestible_questions(user_id):
