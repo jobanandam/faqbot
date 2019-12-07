d97e20bbba17c72f2632f6af0301161a1251f131
2019-Q2_SOW_DEV_031 <2019-Q2_SOW_DEV_031@gmail.com>
2019-12-07 14:07:03 +0100

    created function to append json data into existing array in a file

--- application/file_writer.py
+++ application/file_writer.py
@@ -1,4 +1,7 @@
+import json
 from uuid import uuid4
+
+from application.file_reader import read_json_file
 
 
 def write_file(target, content, mode):
@@ -9,14 +12,29 @@ def write_file(target, content, mode):
     file.close()
 
 
+def append_json_into_array_file(data, file_name):
+    tuples = read_json_file(file_name)
+    obj_list = list(tuples)
+    obj_list.append(data)
+    tuples = tuple(obj_list)
+    write_json_file(tuples, file_name)
+
+
+def write_json_file(json_data, target_file):
+    file = open(target_file, "w")
+    json.dump(json_data, file)
+
+
 def main():
-    data = {"user_id": str(uuid4()), "suggestible_questions": [
+    data = {str(uuid4()): {"suggestible_questions": [
                 {"answer": "How are you1?", "processed": "N"},
                 {"answer": "How are you2?", "processed": "N"},
                 {"answer": "How are you3?", "processed": "N"},
                 {"answer": "How are you4?", "processed": "N"}
-            ]}
-    print(write_file("./resources/test.txt", data, "a"))
+            ]}}
+    append_json_into_array_file(data, "./resources/test.txt")
+
+    # print(write_file("./resources/test.txt", data, "a"))
 
 
 if __name__ == '__main__':
