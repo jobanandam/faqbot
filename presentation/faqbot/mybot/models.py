b9ec347601eff04ebd07ff60c35b3a42f835e24e
vijayvigneshselvaraj <vijayvigneshselvaraj@gmail.com>
2020-04-22 11:51:12 +0200

    addeed models for QA and Categories

--- presentation/faqbot/mybot/models.py
+++ presentation/faqbot/mybot/models.py
@@ -4,3 +4,22 @@ from django.db import models
 class ChatModel(models.Model):
     type = models.CharField(max_length=200)
     text = models.CharField(max_length=200)
+
+class CategoryModel(models.Model):
+    category = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.category
+
+class QuestionAnswerModel(models.Model):
+    question = models.TextField()
+    answer = models.TextField()
+    category = models.ForeignKey(CategoryModel, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Q&A'
+        verbose_name_plural = 'Q&A'
