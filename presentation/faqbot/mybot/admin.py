0fc5458ae422efd4bbacd82cc7942362ce8caa2a
vijayvigneshselvaraj <vijayvigneshselvaraj@gmail.com>
2020-04-22 11:52:19 +0200

    registered the qa and category models to admin page

--- presentation/faqbot/mybot/admin.py
+++ presentation/faqbot/mybot/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import ChatModel
+from .models import CategoryModel, QuestionAnswerModel
 
-admin.site.register(ChatModel)
+admin.site.register(CategoryModel)
+admin.site.register(QuestionAnswerModel)
