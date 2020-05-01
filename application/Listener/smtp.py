d1cf09bc120e04a152eea1317806bfd808c629f9
hasheas <hasheas@gmail.com>
2020-05-01 19:00:19 +0200

    Adding a sample SMTP mail

---
+++ application/Listener/smtp.py
@@ -0,0 +1,31 @@
+import smtplib, ssl
+
+smtp_server = "smtp.gmail.com"
+port = 587  # For starttls
+sender_email = "faqbotlistener@gmail.com"
+receiver_email = "faqbotlistener@gmail.com"
+password = "faqbotlisten"
+
+message = """\
+Subject: Web Scrapping Notification
+
+We have noticed a change in the website.
+Please goto Admin portal to re initiate the scrapping for this website.
+- Powered by FaqBot-Python-Listener"""
+
+# Create a secure SSL context
+context = ssl.create_default_context()
+
+# Try to log in to server and send email
+try:
+    server = smtplib.SMTP(smtp_server, port)
+    server.ehlo()  # Can be omitted
+    server.starttls(context=context)  # Secure the connection
+    server.ehlo()  # Can be omitted
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
+except Exception as e:
+    # Print any error messages to stdout
+    print(e)
+finally:
+    server.quit()
